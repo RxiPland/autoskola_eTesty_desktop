8714692e09bf8829744222dab2f71807792a385f
RxiPland <liborvanura@gmail.com>
2023-04-01 18:04:07 +0200

    added file checking function

--- autoskola_eTesty/main.cpp
+++ autoskola_eTesty/main.cpp
@@ -2,11 +2,60 @@
 #include "startmenu.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QDir>
+#include <QJsonObject>
+#include <QJsonDocument>
 
+
+QString appVersion = "v1.0.0";
+
+
+void checkStatsFile(){
+    // check file with stats
+
+    QFile statsFile(QDir::currentPath() + "/Data/stats.json");
+
+    if(statsFile.exists()){
+
+        statsFile.open(QIODevice::ReadOnly | QIODevice::Text);
+
+        QByteArray fileContent = statsFile.readAll();
+        statsFile.close();
+
+        if(fileContent.isEmpty()){
+            // File is empty
+
+        } else{
+            QJsonObject loadedJson = QJsonDocument::fromJson(fileContent).object();
+
+            if(loadedJson.isEmpty()){
+                // JSON is corrupted
+
+            } else{
+                // OK
+
+                return;
+            }
+        }
+    }
+
+
+    QDir directory(QDir::currentPath());
+    directory.mkdir("./Data");
+
+    statsFile.open(QIODevice::WriteOnly | QIODevice::Text);
+    statsFile.write("{}");
+    statsFile.close();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // check if json is not corrupted
+    checkStatsFile();
+
     StartMenu w;
     w.show();
     return a.exec();
