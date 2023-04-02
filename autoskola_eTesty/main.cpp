05db900c2659ad8a6341be64216c39256cf74ae0
RxiPland <liborvanura@gmail.com>
2023-04-02 12:23:14 +0200

    checking files integrity

--- autoskola_eTesty/main.cpp
+++ autoskola_eTesty/main.cpp
@@ -11,42 +11,104 @@
 QString appVersion = "v1.0.0";
 
 
-void checkStatsFile(){
-    // check file with stats
+void checkFilesIntegrity(){
+    // check stats and settings file
 
+    QJsonObject loadedJson;
+    QByteArray fileContent;
+    QDir directory(QDir::currentPath());
+
+    bool corrupted = false;
+
+
+    // STATS FILE INTEGRITY
     QFile statsFile(QDir::currentPath() + "/Data/stats.json");
 
     if(statsFile.exists()){
 
         statsFile.open(QIODevice::ReadOnly | QIODevice::Text);
 
-        QByteArray fileContent = statsFile.readAll();
+        fileContent = statsFile.readAll();
         statsFile.close();
 
         if(fileContent.isEmpty()){
             // File is empty
+            corrupted = true;
 
         } else{
-            QJsonObject loadedJson = QJsonDocument::fromJson(fileContent).object();
+            loadedJson = QJsonDocument::fromJson(fileContent).object();
 
             if(loadedJson.isEmpty()){
                 // JSON is corrupted
+                corrupted = true;
 
             } else{
                 // OK
-
-                return;
+                corrupted = false;
             }
         }
+
+    } else{
+        // file does not exists
+        corrupted = true;
     }
 
 
-    QDir directory(QDir::currentPath());
-    directory.mkdir("./Data");
+    if(corrupted){
+        directory.mkdir("./Data");
 
-    statsFile.open(QIODevice::WriteOnly | QIODevice::Text);
-    statsFile.write("{}");
-    statsFile.close();
+        statsFile.open(QIODevice::WriteOnly | QIODevice::Text);
+        statsFile.write("{}");
+        statsFile.close();
+    }
+
+
+    // SETTINGS FILE INTEGRITY
+    QFile settingsFile(QDir::currentPath() + "/Data/settings.json");
+
+    if(settingsFile.exists()){
+
+        settingsFile.open(QIODevice::ReadOnly | QIODevice::Text);
+
+        fileContent = settingsFile.readAll();
+        settingsFile.close();
+
+        if(fileContent.isEmpty()){
+            // File is empty
+            corrupted = true;
+
+        } else{
+            loadedJson = QJsonDocument::fromJson(fileContent).object();
+
+            if(loadedJson.isEmpty()){
+                // JSON is corrupted
+                corrupted = true;
+
+            } else{
+                // OK
+                corrupted = false;
+            }
+        }
+
+    } else{
+        // file does not exists
+        corrupted = true;
+    }
+
+
+    if(corrupted){
+        directory.mkdir("./Data");
+
+        QJsonObject newJson;
+        newJson["user_agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+
+        QJsonDocument docData(newJson);
+
+        settingsFile.open(QIODevice::WriteOnly | QIODevice::Text);
+        settingsFile.write(docData.toJson());
+        settingsFile.close();
+    }
+    //corrupted = false;
 }
 
 int main(int argc, char *argv[])
@@ -54,7 +116,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     // check if json is not corrupted
-    checkStatsFile();
+    checkFilesIntegrity();
 
     StartMenu w;
     w.show();
