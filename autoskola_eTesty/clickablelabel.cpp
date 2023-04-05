6444e43e15bad0e9dac58b97e97e6f321fab7f4b
RxiPland <liborvanura@gmail.com>
2023-04-05 19:40:39 +0200

    clickable label

---
+++ autoskola_eTesty/clickablelabel.cpp
@@ -0,0 +1,19 @@
+
+#include "clickablelabel.h"
+
+ClickableLabel::ClickableLabel(QWidget *parent, Qt::WindowFlags f)
+    : QLabel{parent}
+{
+
+}
+
+ClickableLabel::~ClickableLabel()
+{
+
+}
+
+void ClickableLabel::mousePressEvent(QMouseEvent *event)
+{
+    emit clicked();
+}
+
