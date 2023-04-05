42c0eaf1e87f51c9ebe976e1e853fa6a735353a8
RxiPland <liborvanura@gmail.com>
2023-04-06 00:19:49 +0200

    new setting

--- autoskola_eTesty/main.cpp
+++ autoskola_eTesty/main.cpp
@@ -110,6 +110,7 @@ void checkFilesIntegrity(){
 
         QJsonObject newJson;
         newJson["user_agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+        newJson["waiting_interval_miliseconds"] = 1000;
 
         QJsonDocument docData(newJson);
 
