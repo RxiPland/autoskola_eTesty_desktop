a161fe2a9204e60a0919d2afedbcf7d934c12046
RxiPland <liborvanura@gmail.com>
2023-04-06 17:44:22 +0200

    added settings window

---
+++ autoskola_eTesty/ui_settingsdialog.h
@@ -0,0 +1,46 @@
+/********************************************************************************
+** Form generated from reading UI file 'settingsdialog.ui'
+**
+** Created by: Qt User Interface Compiler version 6.4.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_SETTINGSDIALOG_H
+#define UI_SETTINGSDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_SettingsDialog
+{
+public:
+
+    void setupUi(QDialog *SettingsDialog)
+    {
+        if (SettingsDialog->objectName().isEmpty())
+            SettingsDialog->setObjectName("SettingsDialog");
+        SettingsDialog->resize(240, 320);
+
+        retranslateUi(SettingsDialog);
+
+        QMetaObject::connectSlotsByName(SettingsDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *SettingsDialog)
+    {
+        SettingsDialog->setWindowTitle(QCoreApplication::translate("SettingsDialog", "Dialog", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class SettingsDialog: public Ui_SettingsDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_SETTINGSDIALOG_H
