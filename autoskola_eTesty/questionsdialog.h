d11cf356d55f66c4ffc931299d8bec40e8fc986a
RxiPland <liborvanura@gmail.com>
2023-04-07 14:15:22 +0200

    beutified

--- autoskola_eTesty/questionsdialog.h
+++ autoskola_eTesty/questionsdialog.h
@@ -44,10 +44,10 @@ private slots:
 
     void on_pushButton_clicked();
     void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
 
     void on_checkBox_clicked();
 
-    void on_pushButton_3_clicked();
 
 private:
     Ui::QuestionsDialog *ui;
